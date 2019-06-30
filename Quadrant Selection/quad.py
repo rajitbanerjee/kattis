3b2732cb46d21e1f5870af3d9aaa844f80685455
rajitbanerjee <rajit.banerjee@ucdconnect.ie>
2019-06-30 09:41:48 +0530

    Quadrant Selection

---
+++ Quadrant Selection/quad.py
@@ -0,0 +1,11 @@
+"""https://open.kattis.com/problems/quadrant"""
+
+x, y = int(input()), int(input())
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)
+elif x < 0 and y < 0:
+    print(3)
+else:
+    print(4)
